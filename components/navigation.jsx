e79e25bc23e22adf9d4ddca996aa695b159a9853
William Magalhães Rodrigues <williamrodrigues@cvccorp.com.br>
2021-02-16 22:01:00 -0300

    - Loading head in react

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -1,3 +1,23 @@
-function Navigation() {
-    return (<div>Navigation</div>);
+function Navigation({
+    linkIndex,
+    logo,
+    logoAlt,
+    borderless,
+    children
+}) {
+    return (
+        <div id="page-header">
+            <div className="headerbar">
+                <div id="headerbar-top">
+                    <div className="wrap">
+                        <a href={linkIndex} id="logo"><img loading="lazy" src={logo} alt={logoAlt}/></a>
+
+                        <ul className={`navbar navlinks${borderless}`}>
+                            <li>{children}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,5 +1,11 @@
 function HomePage() {
     return (<div className="page-home">
-        <Navigation />
+        <Navigation
+            linkIndex={window.U_INDEX}
+            logo={window.LOGO}
+            logoAlt={window.L_INDEX}
+            borderless={window.NAVBAR_BORDERLESS}>
+            {window.GENERATED_NAV_BAR}
+        </Navigation>
     </div>);
 }
