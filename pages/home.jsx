0d0abb31895e2ec7841f1e06aaaa6d6cc4a67ca5
William Magalhães Rodrigues <williamrodrigues@cvccorp.com.br>
2021-02-16 22:07:23 -0300

    - Adjusting parameters in header

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,11 +1,11 @@
 function HomePage() {
     return (<div className="page-home">
         <Navigation
-            linkIndex={window.U_INDEX}
-            logo={window.LOGO}
-            logoAlt={window.L_INDEX}
-            borderless={window.NAVBAR_BORDERLESS}>
-            {window.GENERATED_NAV_BAR}
+            linkIndex={window.header.U_INDEX}
+            logo={window.header.LOGO}
+            logoAlt={window.header.L_INDEX}
+            borderless={window.header.NAVBAR_BORDERLESS}>
+            {window.header.GENERATED_NAV_BAR}
         </Navigation>
     </div>);
 }
